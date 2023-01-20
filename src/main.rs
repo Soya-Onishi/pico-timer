dc679e2d42e395a809d17a06dd2e7805ac2e4240
Soya Ohnishi <soya.ohnishi@gmail.com>
2023-01-20 22:09:16 +0900

    Implement led blink based on Timer

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,38 @@
-//! Blinks the LED on a Pico board
-//!
-//! This will blink an LED attached to GP25, which is the pin the Pico uses for the on-board LED.
 #![no_std]
 #![no_main]
 
-use bsp::entry;
+use bsp::{entry, hal::timer::Alarm};
 use defmt::*;
 use defmt_rtt as _;
-use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::digital::v2::ToggleableOutputPin;
 use panic_probe as _;
 
-// Provide an alias for our BSP so we can switch targets quickly.
-// Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
 use rp_pico as bsp;
-// use sparkfun_pro_micro_rp2040 as bsp;
 
-use bsp::hal::{
-    clocks::{init_clocks_and_plls, Clock},
-    pac,
-    sio::Sio,
-    watchdog::Watchdog,
-};
+use bsp::hal::{clocks::init_clocks_and_plls, gpio, pac, sio::Sio, timer, watchdog};
+
+use gpio::{pin::bank0::Gpio25, pin::Output, pin::PushPull};
+
+use pac::interrupt;
+
+use core::cell::RefCell;
+use core::ops::DerefMut;
+use cortex_m::interrupt::{free, Mutex};
+
+use fugit::MicrosDurationU32;
+
+static ALARM0: Mutex<RefCell<Option<timer::Alarm0>>> = Mutex::new(RefCell::new(None));
+static LED: Mutex<RefCell<Option<gpio::Pin<Gpio25, Output<PushPull>>>>> =
+    Mutex::new(RefCell::new(None));
 
 #[entry]
 fn main() -> ! {
     info!("Program start");
     let mut pac = pac::Peripherals::take().unwrap();
-    let core = pac::CorePeripherals::take().unwrap();
-    let mut watchdog = Watchdog::new(pac.WATCHDOG);
     let sio = Sio::new(pac.SIO);
-
-    // External high-speed crystal on the pico board is 12Mhz
-    let external_xtal_freq_hz = 12_000_000u32;
-    let clocks = init_clocks_and_plls(
-        external_xtal_freq_hz,
+    let mut watchdog = watchdog::Watchdog::new(pac.WATCHDOG);
+    init_clocks_and_plls(
+        bsp::XOSC_CRYSTAL_FREQ,
         pac.XOSC,
         pac.CLOCKS,
         pac.PLL_SYS,
@@ -44,8 +43,6 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
-
     let pins = bsp::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
@@ -53,16 +50,41 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    let mut led_pin = pins.led.into_push_pull_output();
+    let led_pin = pins.led.into_push_pull_output();
 
-    loop {
-        info!("on!");
-        led_pin.set_high().unwrap();
-        delay.delay_ms(500);
-        info!("off!");
-        led_pin.set_low().unwrap();
-        delay.delay_ms(500);
+    let mut timer = timer::Timer::new(pac.TIMER, &mut pac.RESETS);
+    let mut alarm0 = timer.alarm_0().unwrap();
+
+    free(|cs| {
+        alarm0.enable_interrupt();
+        alarm0.clear_interrupt();
+        alarm0
+            .schedule(MicrosDurationU32::from_ticks(1_000_000))
+            .unwrap();
+        ALARM0.borrow(cs).replace(Some(alarm0));
+        LED.borrow(cs).replace(Some(led_pin));
+    });
+
+    unsafe {
+        pac::NVIC::unmask(pac::Interrupt::TIMER_IRQ_0);
     }
+
+    loop {}
 }
 
-// End of file
+#[interrupt]
+fn TIMER_IRQ_0() {
+    free(|cs| {
+        if let (Some(alarm0), Some(led)) = (
+            ALARM0.borrow(cs).borrow_mut().deref_mut(),
+            LED.borrow(cs).borrow_mut().deref_mut(),
+        ) {
+            alarm0.clear_interrupt();
+            alarm0
+                .schedule(MicrosDurationU32::from_ticks(1_000_000))
+                .unwrap();
+
+            led.toggle().unwrap();
+        }
+    })
+}
